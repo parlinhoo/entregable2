719fa53983a38396f70386c873016a61d3ec91df
Fabian G <fgonzalezuribe18@gmail.com>
2024-05-27 15:32:02 -0400

    Agregadas funciones virtual de MapADT

--- clases/MapADT.h
+++ clases/MapADT.h
@@ -2,5 +2,8 @@
 
 template <class key_type, class value_type> class MapADT {
     virtual std::optional<value_type> get(key_type key);
-
+    virtual void put(key_type key, value_type value);
+    virtual std::optional<value_type> remove(key_type key);
+    virtual int size();
+    virtual bool isEmpty();
 };
