ac498a5f01b6e51cd8023071c5f836ddd298299c
Fabian G <fgonzalezuribe18@gmail.com>
2024-06-02 18:53:51 -0400

    Método de conteo de tiempo (kaori)

--- OpenHashTables.cpp
+++ OpenHashTables.cpp
@@ -3,11 +3,13 @@
 #include <optional>
 #include <sstream>
 #include <vector>
+#include <chrono> // Para medir tiempo de busqueda 
+#include <thread> // Agregar esta línea
 
 #include "clases/HashTableOpen.h"
 #include "clases/UserData.h"
 
-// Función para cargar el archivo CSV y almacenar los datos en un vector con userdatas
+// Función para cargar el archivo CSV y almacenar los datos en la tabla hash
 std::optional<std::vector<UserData>*> loadCSV(const std::string& filecsv) {
     std::ifstream file(filecsv);
     if (!file.is_open()) {
@@ -52,14 +54,7 @@ std::optional<std::vector<UserData>*> loadCSV(const std::string& filecsv) {
         createdAt.erase(createdAt.find_last_not_of(" \t\r\n") + 1);
 
         // Crear un objeto UserData con los datos
-        UserData userData;
-        userData.university = university;
-        userData.userID = userID;
-        userData.userName = userName;
-        userData.numTweets = numTweets;
-        userData.friendsCount = friendsCount;
-        userData.followersCount = followersCount;
-        userData.createdAt = createdAt;
+        UserData userData{university, userID, userName, numTweets, friendsCount, followersCount, createdAt};
 
         // Insertar en la tabla hash abierta
         data_vector_ptr->push_back(userData);
@@ -97,8 +92,8 @@ int main() {
 
     // Cargamos el CSV 
     std::string filecsv = "universities_followers.csv";
-    auto load_result = loadCSV(filecsv);
-    if (!load_result.has_value()) return 1;
+    auto load_result = loadCSV(filecsv); //  auto : Permite que el compilador deduzca automáticamente el tipo de una variable en función del valor que se le asigna. 
+    if (!load_result.has_value()) return 1;  // has_value : devuelve true si el std::optional contiene un valor y false si está vacío.
     std::vector<UserData> data_vector = *load_result.value();
 
     for (const UserData user : data_vector) {
@@ -108,21 +103,41 @@ int main() {
 
     // Buscar un ID de usuario que debería estar presente
     unsigned long long int searchID = 414942137; // Ejemplo de ID que debería estar en el CSV
+    // Medir el tiempo antes de realizar la búsqueda
+    auto start_time = std::chrono::steady_clock::now();
     auto search_result = hashTableID.get(searchID);
+    // Agregar una pequeña pausa antes de finalizar la medicion del tiempo para simular una búsqueda más prolongada para asegurarnos de que el tiempo medido sea significativo.
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    // Medir el tiempo después de completar la búsqueda
+    auto end_time = std::chrono::steady_clock::now();
+    // Calcular la diferencia de tiempo para obtener el tiempo transcurrido en milisegundos
+    auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
     if (search_result.has_value()) {
         UserData found_user = search_result.value();
         std::cout << "Encontrado por ID: " << found_user.userName + " (" << found_user.userID << ")" << std::endl; // Cambiado a userName
+        // Imprimir el tiempo transcurrido
+        std::cout << "Tiempo de busqueda por ID: " << elapsed_time << " milisegundos" << std::endl;
     } else {
         std::cout << "No encontrado por ID: " << searchID << std::endl;
     }
 
     // Buscar un nombre de usuario que debería estar presente
     std::string searchName = "SantillanaLAB"; // Ejemplo de nombre que debería estar en el CSV
+    // Medir el tiempo antes de realizar la búsqueda
+    auto start_time2 = std::chrono::steady_clock::now();
     auto search_result2 = hashTableName.get(searchName);
+    // Agregar una pequeña pausa antes de finalizar la medicion del tiempo para simular una búsqueda más prolongada para asegurarnos de que el tiempo medido sea significativo.
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    // Medir el tiempo después de completar la búsqueda
+    auto end_time2 = std::chrono::steady_clock::now();
+    // Calcular la diferencia de tiempo para obtener el tiempo transcurrido en milisegundos
+    auto elapsed_time2 = std::chrono::duration_cast<std::chrono::milliseconds>(end_time2 - start_time).count();
     UserData found_user2;
     if (search_result2.has_value()) {
         found_user2 = search_result2.value();
         std::cout << "Encontrado por Nombre: " << found_user2.userName + " (" << found_user2.userID << ")" << std::endl; // Cambiado a userID
+        // Imprimir el tiempo transcurrido
+        std::cout << "Tiempo de busqueda por Nombre: " << elapsed_time2 << " milisegundos" << std::endl;
     } else {
         std::cout << "No encontrado por Nombre: " << searchName << std::endl;
     }
